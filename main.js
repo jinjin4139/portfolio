ca1cf7d4426cb30f26a28587afb9e79fcc981e63
jinjin4139 <3825956@naver.com>
2023-01-10 00:44:31 +0900

    JS_Final

--- main.js
+++ main.js
@@ -19,7 +19,10 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
   scrollIntoView(link);
+  selectNavItem(target);
+
 });
 
 // Navbar toggle button for small screen 알려주셨나?
@@ -44,12 +47,12 @@ document.addEventListener('scroll', ()=> {
 })
 
 //show 'arrow up' button when scrolling down
-const arrowUp = document.querySelector('.arrow-up')
+const arrowUp = document.querySelector('.arrow-up');
 document.addEventListener('scroll', ()=> {
   if(window.scrollY > homeheight / 2) {
     arrowUp.classList.add('visible');
   } else {
-arrowUp.classList.remove('visible')
+arrowUp.classList.remove('visible');
   }
 });
 //handle click on the 'arrow up' button
@@ -57,6 +60,36 @@ arrowUp.addEventListener('click', () => {
   scrollIntoView('#home');
 });
 
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if (filter == null) {
+    return;
+  }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    if (active != null) {
+      active.classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+  
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+      projects.forEach((project) => {
+        console.log(project.dataset.type);
+        if (filter === '*' || filter === project.dataset.type) {
+          project.classList.remove('invisible');
+        } else {
+          project.classList.add('invisible');
+        }
+      });
+      projectContainer.classList.remove('anim-out');
+    }, 300);
+  });
 
 
 
@@ -69,4 +102,58 @@ function scrollIntoView(selector) {
   scrollTo.scrollIntoView({behavior: 'smooth'});
 }
 
+//1. 모든섹션 요소들을 가지고 온다
+//2. IntersectionObserver를 이용해서 모든섹션들을 관찰한다
+//3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
 
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact',  
+];
+const section = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => document.querySelector(`[data-link="${id}"]`));
+
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+      selectedNavItem.classList.remove('active');
+      selectedNavItem = selected;
+      selectedNavItem.classList.add('active');
+}
+const observerOptions= {
+  root:null,
+  rootMargin: '0px',
+  threshold:0.3,
+
+
+}
+const observerCallback = (entries, observer) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting &&entry.intersectionRatio > 0) {
+      const index =  sectionIds.indexOf(`#${entry.target.id}`);
+//스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+      
+    }
+  });
+};
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+section.forEach((section) => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+  if(window.scrollY ===0) {
+    selectedNavIndex = 0;
+  } else if(Window.scrollY + window.innerHeight === document.body.clientHeight)
+  {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
+});
